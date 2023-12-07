051a7ac2f8a64912e1e34ff1038185e1c7e2a6b4
CodyCardinal23 <codycardinal@gmail.com>
2023-12-06 21:47:06 -0700

    Another checkin on this branch

--- src/com/coderscampus/ExampleApp.java
+++ src/com/coderscampus/ExampleApp.java
@@ -4,6 +4,8 @@ public class ExampleApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Testing 123");
+		System.out.println("Testing some more");
 
 	}
 
